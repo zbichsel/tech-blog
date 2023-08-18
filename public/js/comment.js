6ec7a3c1f4c22744d56425b932a411393cec08d8
Zackrey Bichsel <biksel09@hotmail.com>
2023-08-17 21:57:01 -0500

    fixed comment.js and added fetch call to logout.js with click event

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,14 +1,25 @@
-const logout = async () => {
-    const response = await fetch('/api/users/logout', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json'},
-    });
+const commentButtonHandler = async (event) => {
+    event.preventDefault();
 
-    if (response.ok) {
-        document.location.replace('/');
-    } else {
-        alert (response.statusText);
+    const content = document.querySelector('textarea[name ="commentText"]').value;
+    const blog_id = document.querySelector('input[name="blog-id"]').value;
+    const date_created = new Date().toLocaleDateString();
+
+    if (content && blog_id && date_created) {
+        const response = await fetch(`/api/comments`, {
+            method: 'POST',
+            body: JSON.stringify({ content, blog_id, date_created }),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+
+        if (response.ok) {
+            document.location.reload();
+        } else {
+            alert('Could not create comment.');
+        }
     }
 };
 
-document.querySelector('#logout').addEventListener('click', logout);
+document.getElementById('addComment').addEventListener('click', commentButtonHandler);
